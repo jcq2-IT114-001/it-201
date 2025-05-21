7020406c6d01a5445f7e7ab16e8f8fec8da857b1
Jose C. Quispe Ranilla <144377864+jcq2-IT114-001@users.noreply.github.com>
2025-05-20 20:24:21 -0400

    Create update_chat.php
    url: https://web.njit.edu/~jcq2/project5/index.php

---
+++ project5/update_chat.php
@@ -0,0 +1,31 @@
+<?php
+$con = mysqli_connect("sql1.njit.edu", "jcq2", "Davidranilla3#", "jcq2");
+
+if (!$con) {
+    echo "DB error";
+    exit;
+}
+
+if (!isset($_POST['name']) || !isset($_POST['password']) || !isset($_POST['content'])) {
+    echo "Missing data";
+    exit;
+}
+
+$n = $_POST['name'];
+$p = $_POST['password'];
+$c = $_POST['content'];
+
+$sql = "UPDATE chat SET content=? WHERE name=? AND password=?";
+$stmt = mysqli_prepare($con, $sql);
+mysqli_stmt_bind_param($stmt, "sss", $c, $n, $p);
+mysqli_stmt_execute($stmt);
+
+if (mysqli_stmt_affected_rows($stmt) > 0) {
+    echo "Updated";
+} else {
+    echo "Failed";
+}
+
+mysqli_stmt_close($stmt);
+mysqli_close($con);
+?>
